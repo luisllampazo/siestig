447f0b85016e43a89257c5aa64c460bf7939b710
luis.llampazo@gmail.com <SinBandera*2>
2020-06-03 14:49:18 -0500

    lazy load

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,7 @@
 // JavaScript Document
 $('.accordion-toggle').click(function(){
 	$('.hiddenRow').hide();
+//alert($(this).next('tr').text());
 	$(this).next('tr').find('.hiddenRow').show();
 });
+
